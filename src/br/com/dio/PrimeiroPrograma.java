229bdd18a5d66a770278bc9dd981413009991e27
Juliano <202111041529@alunos.estacio.br>
2021-12-27 10:58:36 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,7 +2,44 @@ package br.com.dio;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("hello World");
 
+        Livro livro1 = new Livro("O Corvo", 100);
+        System.out.println(livro1);
     }
 }
+    class Livro{
+        private String nome;
+        private Integer npag;
+
+        public Livro() {
+        }
+
+        public Livro(String nome, Integer npag) {
+            this.nome = nome;
+            this.npag = npag;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNpag() {
+            return npag;
+        }
+
+        public void setNpag(Integer npag) {
+            this.npag = npag;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", npag=" + npag +
+                    '}';
+            }
+    }
